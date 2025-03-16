f94413a9f40d5e12d925e0e78f9004e626c2515b
Jakub Stelmasiak <stelmon3@gmail.com>
2025-03-16 11:49:47 +0100

    add middleware

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//ednpoints
+
 app.listen(8000, () =>{
     console.log('Server is running on port: 8000')
 })
