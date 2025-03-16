e50276295c2ab503993ea0c774b45c724d269ec0
Jakub Stelmasiak <stelmon3@gmail.com>
2025-03-16 12:42:02 +0100

    add db

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const db = [
+    { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+    { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+  ];
+
 //ednpoints
 
 app.listen(8000, () =>{
