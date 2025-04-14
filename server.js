f122d566eb5667d8b253e023d60cdeb68c6123c0
Jakub Stelmasiak <stelmon3@gmail.com>
2025-04-14 21:25:26 +0200

    add seats endpoints

--- server.js
+++ server.js
@@ -87,7 +87,12 @@ app.get('/concerts/random', (req, res) => {
 app.get('/concerts/:id', (req, res) => {
     const id = req.params.id
     const concert = db.concerts.findIndex(item => String(item.id) === String(id)); // znajdz mi item z takim item.id (concerts/2 to jego item.id jest 2) jak id (czyli wlasnie np concerts/2)
-    res.json(db.concerts[concert]);
+    
+    if (!concert){
+        res.json({ message: 'wrong id!'});
+    } else {
+        res.json(db.concerts[concert]);
+    }
 });
 
 app.post('/concerts', (req, res) => {
@@ -132,7 +137,63 @@ app.delete('/concerts/:id', (req, res) => {
 
 app.get('/seats', (req, res) => {
     res.json(db.seats);
-})
+});
+
+app.get('/seats/random', (req, res) => {
+    const randomSeat = db.seats[Math.floor(Math.random() * db.seats.length)];
+    res.json(randomSeat);
+});
+
+app.get('/seats/:id', (req, res) => {
+    const id = req.params.id;
+    const seat = db.seats.findIndex(item => String(item.id) === String(id));
+    if (!seat){
+        res.json({ message: 'wrong id!'});
+    } else {
+        res.json(db.seats[seat]);
+    }
+});
+
+app.post('/seats', (req, res) => {
+    const id = uuidv4();
+    const { day, seat, client, email } = req.body
+
+    if (!day || !seat || !client || !day || !email || !id){
+        res.json({ message: 'error!'});
+    } else {
+        db.seats.push({ day, seat, client, email, id })
+        res.json({ message: 'OK' })
+    }
+    
+});
+
+app.put('/seats/:id', (req, res) => {
+    const id = req.params.id;
+    const seatIndex = db.seats.findIndex(item => String(item.id) === String(id));
+    const { day, seat, client, email } = req.body;
+
+    if (!seatIndex){
+        res.status(404).json({ message: 'seat not found!'});
+    } 
+    
+    if (!day || !seat || !client || !day || !email || !id){
+        res.json({ message: 'error!'});
+    }
+        db.seats[seatIndex] = {...db.seats[seatIndex], day, seat, client, email}
+        res.json({ message: 'OK' })
+});
+
+app.delete('/seats/:id', (req, res) => {
+    const id = req.params.id;
+    const seat = db.seats.findIndex(item => String(item.id) === String(id));
+
+    if(seat !== -1) {
+        db.seats.splice(seat, 1);
+        res.json({ message: 'OK' });
+    } else {
+        res.status(404).json({ message: 'Seat not found' });
+    }
+});
 
 app.listen(8000, () =>{
     console.log('Server is running on port: 8000')
