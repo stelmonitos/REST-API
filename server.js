2efc82fd875c8f30f289702de8ed91b3e0aed26d
Jakub Stelmasiak <stelmon3@gmail.com>
2025-05-14 20:19:00 +0200

    fix frontend middleware

--- server.js
+++ server.js
@@ -12,10 +12,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+
 // ENDPOINTS
 app.use('/api', testimonialRoutes);
 app.use('/api', concertRoutes);
 app.use('/api', seatRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.listen(8000, () =>{
     console.log('Server is running on port: 8000')
